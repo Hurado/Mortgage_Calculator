454477418b7baf5ffb79c633d16316c4079d0b49
Hurado <piaszczynskiarkadiusz@gmail.com>
2022-12-03 20:58:13 +0100

    feat(project): Added a validation to program for inputs values.

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,36 @@ public class Main {
       final byte MONTHS_IN_YEAR = 12;
       final byte PERCENT = 100;
 
-
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal:");
-        int principal = scanner.nextInt();
+        int principal=0;
+        float monthlyInterest=0;
+        int numberOfPayments=0;
+        while (true) {
+          System.out.print("Principal:");
+          principal = scanner.nextInt();
+          if(principal>=1000 && principal<=1_000_000)
+            break;
+          System.out.println("Enter a valid value between 1k$ and 1kk$");
+        }
+        while (true) {
+          System.out.print("Annual Interest Rate: ");
+          float annualInterest = scanner.nextFloat();
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest/PERCENT/MONTHS_IN_YEAR;
-
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
-
+          if (annualInterest>=1 && annualInterest<=30) {
+            monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+            break;
+          }
+          System.out.println("Enter a valid value between 1 and 30.");
+        }
+        while(true) {
+          System.out.print("Period (Years): ");
+          byte years = scanner.nextByte();
+          if (years>=1 && years<=30) {
+            numberOfPayments = years * MONTHS_IN_YEAR;
+            break;
+          }
+          System.out.println("Enter a number between 1 and 30");
+        }
         double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest,numberOfPayments)/(Math.pow(1+monthlyInterest,numberOfPayments)-1));
 
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
